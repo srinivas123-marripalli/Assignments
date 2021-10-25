4357f0b0195c629df6ce511a7758f74df32c7d69
srinivas123-marripalli <59600271+srinivas123-marripalli@users.noreply.github.com>
2021-10-25 23:31:12 +0530

    useRef(functional component)

--- App.js
+++ App.js
@@ -1,39 +1,29 @@
-import React,{useState} from "react"
-function App(){
-const[products,setProducts] = useState([
-  {"p_id":111,"p_name":"p_one","p_cost":10000},
-  {"p_id":222,"p_name":"p_two","p_cost":20000},
-  {"p_id":333,"p_name":"p_three","p_cost":30000},
-  {"p_id":444,"p_name":"p_four","p_cost":40000}
-]);
-const add_products = ()=>{
-  setProducts(
-  [...products,{"p_id":5555,"p_name":"p_five","p_cost":50000}]
-)
-};
-return(
-  <React.Fragment>
-    <table border="1" align="center" cellpadding="10" cellspacing="10">
-      <thead>
-        <tr>
-        <th>SNO</th>
-        <th>NAME</th>
-        <th>COST</th>
-        </tr>
-      </thead>
-      <tbody>
-        
-        {products.map((element,index)=>(
-          <tr key = {index}>
-            <td>{element.p_id}</td>
-            <td>{element.p_name}</td>
-            <td>{element.p_cost}</td>
-        </tr>
-        ))}
-      </tbody>
-    </table>
-    <button onClick={add_products}>ADD PRODUCTS</button>
-  </React.Fragment>
-)
-};
+
+import React, { useRef, useState } from "react";
+function App() {
+  
+  const uemail = useRef();
+  const upassword = useRef();
+
+  const [status,setStatus] = useState("");
+
+  const login = ()=>{
+     if(uemail.current.value === "hr@ashokit.in" && upassword.current.value === "ashokit"){
+        setStatus("Login Success");
+     }else{
+      setStatus("Login Fail");
+     }
+  };
+
+  return (
+      <React.Fragment>
+           <input type="email" ref={uemail}></input>
+           <input type="password" ref={upassword}></input>
+           <button onClick={login}>Login</button>
+           <br></br>
+           <h1>{status}</h1>
+      </React.Fragment>
+  );
+}
+
 export default App;
